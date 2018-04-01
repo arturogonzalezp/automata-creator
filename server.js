4c11307e7fa2069a5fd6369751ae85af200711af
César Arturo González <a01261064@itesm.mx>
2018-04-01 00:37:00 -0600

    Almost done (added design)

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.static('app'));
 app.use(express.static('node_modules'));
 
 app.get('*', function (req, res) {
-    res.sendFile(__dirname + '/app/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(port, () => console.log('Example app listening on port ' + port + '!'))
